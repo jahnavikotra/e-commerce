c3dc6c90ee23c0c2bb423c96b932ddfa8174debc
kotra jahnvi <jahnavikotra49@gmail.com>
2024-11-13 20:00:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const App = () => {
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Shopcategory from './pages/Shopcategory';
+import Loginsignup from './pages/Loginsignup';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Shop from './pages/Shop';
+function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <BrowserRouter>
+       <Navbar/> 
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<Shopcategory category="mens"/>}/>
+        <Route path='/womens' element={<Shopcategory category="women"/>}/>
+        <Route path='/kids' element={<Shopcategory category="kid"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=":productId" element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Loginsignup/>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
-export default App
+export default App;
